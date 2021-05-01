f4967676d154e980e4001cbf2520e0466ddedf5a
Filipe Barbosa <up201909573@edu.fe.up.pt>
2021-05-01 10:58:42 +0100

    Fix server message check
    The check to see if a message is a server message was too late.
    It is now right at the beginning.

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.author.bot)
+    if (message.guild !== null || message.author.bot)
         return;
 
     if (message.content.length === 0 || !message.content.trim()) {
@@ -16,44 +16,40 @@ client.on('message', message => {
         return;
     }
 
-    if (message.guild === null) {
+    try {
+        let guildIds = client.guilds.cache.map(guild => guild.id);
 
-
-        try {
-            let guildIds = client.guilds.cache.map(guild => guild.id);
-
-            if (!guildIds || guildIds.length !== 1) {
-                throw `Number of guilds is ${guildIds.length}`;
-            }
-
-            let guild = client.guilds.cache.get(guildIds[0]);
-
-            if (!guild) {
-                throw `Couldn't get server`;
-            }
-
-            guild.members.fetch(message.author.id)
-                .then(() => {
-                    guild.members.fetch(process.env.CEO_ID)
-                        .then((ceo) => {
-                            const embed = new Discord.MessageEmbed()
-                                .setTitle('Anonymous Message Received')
-                                .setDescription(message.content)
-                                .setTimestamp();
-
-                            ceo.send(embed);
-                            message.author.send("Your anonymous message has been sent.");
-                        })
-                        .catch((error) => {
-                            errorMessage(message.author, `Couldn't get CEO (${error})`)
-                        })
-                })
-                .catch((error) => {
-                    errorMessage(message.author, `Couldn't get message author (${error})`)
-                });
-        } catch (error) {
-            errorMessage(message.author, error);
+        if (!guildIds || guildIds.length !== 1) {
+            throw `Number of guilds is ${guildIds.length}`;
         }
+
+        let guild = client.guilds.cache.get(guildIds[0]);
+
+        if (!guild) {
+            throw `Couldn't get server`;
+        }
+
+        guild.members.fetch(message.author.id)
+            .then(() => {
+                guild.members.fetch(process.env.CEO_ID)
+                    .then((ceo) => {
+                        const embed = new Discord.MessageEmbed()
+                            .setTitle('Anonymous Message Received')
+                            .setDescription(message.content)
+                            .setTimestamp();
+
+                        ceo.send(embed);
+                        message.author.send("Your anonymous message has been sent.");
+                    })
+                    .catch((error) => {
+                        errorMessage(message.author, `Couldn't get CEO (${error})`)
+                    })
+            })
+            .catch((error) => {
+                errorMessage(message.author, `Couldn't get message author (${error})`)
+            });
+    } catch (error) {
+        errorMessage(message.author, error);
     }
 });
 
